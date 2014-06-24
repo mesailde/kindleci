fffbfd574820b06113dc2c2e694d0c8279e35917
Mesailde Souza de Oliveira Matias <mesailde@gmail.com>
2014-06-24 09:18:36 -0300

    convert spaces to tabs

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 fetch() {
-        local destfile=downloads/"$1"
-        while [[ ! -f "$destfile" || `md5sum "$destfile"|cut -d\  -f1` != "$2" ]]; do
-                rm "$destfile" 2>/dev/null
-                wget -O "$destfile" "$3" || return $?
-        done
+	local destfile=downloads/"$1"
+	while [[ ! -f "$destfile" || `md5sum "$destfile"|cut -d\  -f1` != "$2" ]]; do
+		rm "$destfile" 2>/dev/null
+		wget -O "$destfile" "$3" || return $?
+	done
 }
