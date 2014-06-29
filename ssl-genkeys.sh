8ff9b698dcf2b6e75f076a7ab0e81b09f8306be0
Mesailde Souza de Oliveira Matias <mesailde@gmail.com>
2014-06-28 21:03:23 -0300

    also create log dir

--- ssl-genkeys.sh
+++ ssl-genkeys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir -p sslkeys
-cd sslkeys || exit $?
-openssl genrsa -des3 -out server.enc.key -passout pass:qwerty123 1024 || exit $?
-openssl rsa -in server.enc.key -out server.key -passin pass:qwerty123 || exit $?
-openssl req -new -key server.key -out server.csr -subj '/' || exit $?
-openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt || exit $?
+set -xe
+mkdir -p sslkeys log
+cd sslkeys
+openssl genrsa -des3 -out server.enc.key -passout pass:qwerty123 1024
+openssl rsa -in server.enc.key -out server.key -passin pass:qwerty123
+openssl req -new -key server.key -out server.csr -subj '/'
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
